ffb32505a8e81257d5bbc2834ea1f866cf6b3b68
dhruvrawat1807-web <dhruvrawat1807@gmail.com>
2025-10-07 09:32:51 +0530

    Create Problem 19.java

---
+++ Test35.pdf/Problem 19.java
@@ -0,0 +1,18 @@
+public class Q19_ArchersRange {
+    static int firstOccurrence(int[] arr, int key, int low, int high) {
+        if (low > high) return -1;
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            int left = firstOccurrence(arr, key, low, mid - 1);
+            return (left == -1) ? mid : left;
+        }
+        if (arr[mid] > key)
+            return firstOccurrence(arr, key, low, mid - 1);
+        return firstOccurrence(arr, key, mid + 1, high);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 2, 3};
+        System.out.println(firstOccurrence(arr, 2, 0, arr.length - 1));
+    }
+}
